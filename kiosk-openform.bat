35e7eb6669b09ccc8a36dbc8f55cbe22e4b14b39
woacademy <36260671+woacademy@users.noreply.github.com>
2018-02-10 20:51:52 +0000

    Add open form script
    This simple batch script opens the form when the kiosk is started.

---
+++ kiosk-openform.bat
@@ -0,0 +1,3 @@
+REM The syntax for the START command requires a title for the first argument, followed by the executable path and finally it's launch params.
+REM Go to https://docs.google.com/forms/d/e/*/formResponse instead of https://docs.google.com/forms/d/e/*/viewform because it forces our user script to load properly first time.
+START "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk https://docs.google.com/forms/d/e/1FAIpQLSdcQ4NielafI_-lTtPdKqKNrKCVj1ZsgO9faHRj8iBfXZ-z_w/formResponse
